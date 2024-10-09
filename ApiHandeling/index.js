3e63dc8aafb9637b8c7e4b52245c80649e5cafd3
farruop <rafayKhanpvt@gmail.com>
2024-10-10 00:42:50 +0530

    created a basic server, and can also perform search query there

---
+++ ApiHandeling/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const fetProducts=async ()=>{
+    try{
+        const result= await fetch('https://api.freeapi.app/api/v1/public/randomproducts').then(result=>result.json())
+        const products=await result.data.data;
+        console.log(products);
+        // const response = await fetch('https://api.freeapi.app/api/v1/public/randomproducts');
+        // const result = await response.json(); // Await for JSON conversion
+        // const products = result.data.data;
+        return products;
+        
+    }catch(err){
+        console.log(err.message);
+        return[];
+        
+    }
+}
+
+
+
+const app= express();
+const port=process.env.PORT || 3000;
+
+
+app.get('/get', async(req, res)=>{
+
+    const products= await fetProducts()
+
+    if(req.query.search){
+        const filterProd= products.filter(product => product.title.toLowerCase().includes(req.query.search.toLowerCase()))
+        res.send(filterProd);
+        return;
+    }
+
+    setTimeout(()=>{
+        res.send(products)
+    },3000)
+    
+})
+
+app.listen(port, ()=>{
+    console.log(`Port is running on ${port}` );
+    
+})
+
