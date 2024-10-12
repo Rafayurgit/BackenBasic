4042f4b346b439e1b8357f137b05b3fba071cae1
farruop <rafayKhanpvt@gmail.com>
2024-10-12 15:18:53 +0530

    debug some errors related to proxy where i forgot to add / and some minor typo errors

--- ApiHandeling/FrontEnd/src/App.jsx
+++ ApiHandeling/FrontEnd/src/App.jsx
@@ -6,23 +6,50 @@ function App() {
   const [products, setProducts]=useState([]);
   const[error, setError]= useState(false);
   const [loading, setLoading]= useState(false);
-  
+  const [search, setSearch]= useState('')
+
+
 
   useEffect(()=>{
+    const controller = new AbortController();
+    
     ;(async()=>{
       try{
-        const response = await axios.get('/api/search');
-        const result=await response.json();
+        setLoading(true);
+        setError(false);
+
+        const response = await axios.get('/api/get?search=' + search, {signal:controller.signal});
+        setProducts(response.data)
+        
+        console.log(response.data);
+        setLoading(false)
       }catch(error){
-  
+        if(axios.isCancel(error)){
+          console.log('Request cancel', error.message)
+          return
+          
+        }
+        console.log(error.message);
+        setError(true)
+        setLoading(false)
+        
       }
+      
+
     })()
-  },[])
+    return () => controller.abort();
+  },[search])
 
   return (
     <>
       <h1>Api Handeling</h1>
+      <input type="text" placeholder='search' 
+      value={search}
+      onChange={(e)=>setSearch(e.target.value)}
+      />
 
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Something went wrong </h1>}
 
       <h2>Number of Products are {products.length}</h2>
     </>
