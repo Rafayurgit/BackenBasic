0bab859f738146ee21dbc54814543424f07f0abf
farruop <rafayKhanpvt@gmail.com>
2024-12-14 20:08:07 +0530

    Add:mongodb connection

---
+++ HotelManagement/src/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const mongoURL= "mongodb://localhost:27017/hotelManagement";
+
+mongoose.connection(mongoURL,{
+    useNewUrlParse:true,
+    useUnifiedTopology:true,
+})
+
+const db = mongoose.connection;
+
+db.on("connected", ()=>{
+    console.log("Connected to MongoDB server");
+    
+})
+
+db.on("err", (err)=>{
+    console.log("Error occured while connecting db", err);
+})
+db.on("disconnect", ()=>{
+    console.log("mongoDb disconnected");
+    
+})
