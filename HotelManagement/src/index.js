a56dab6405969b396c600962380ca6fe5bad3c4a
farruop <rafayKhanpvt@gmail.com>
2024-12-14 17:47:12 +0530

    getting started with new mini backed project

---
+++ HotelManagement/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app= express();
+const PORT=process.env.PORT|| 8080;
+
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.send("Hello world")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listining on ${PORT}`);
+    
+})
