be2e86d06185675a099c86e74d51b3dd423d9329
farruop <rafayKhanpvt@gmail.com>
2024-12-17 21:39:39 +0530

    Add: bodyParse middleware

--- HotelManagement/src/index.js
+++ HotelManagement/src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const db = require("./config/db.js")
+const person= require('./models/person.model.js')
+
+const bodyParser= require("body-parser");
+app.use(bodyParser.json())
+
 const app= express();
 const PORT=process.env.PORT|| 8080;
 
@@ -10,6 +15,8 @@ app.use(express.json())
 app.get("/", (req,res)=>{
     res.send("Hello world")
 })
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is listining on ${PORT}`);
