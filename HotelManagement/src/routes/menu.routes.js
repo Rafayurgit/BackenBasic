8a5c6e6354b0741b4ffac304d198e62516f32964
farruop <rafayKhanpvt@gmail.com>
2024-12-23 21:05:16 +0530

    Add: parametries /menu endpoint

--- HotelManagement/src/routes/menu.routes.js
+++ HotelManagement/src/routes/menu.routes.js
@@ -14,7 +14,22 @@ router.get("/", async(req, res)=>{
     }
 })
 
-
+router.get("/:tasteType", async(req,res)=>{
+    try {
+        const tasteType=req.params.tasteType;
+        if(tasteType==="sweet" || tasteType==="Spicy" || tasteType==="salty"){
+            const response= await MenuItem.find({taste:tasteType});
+            res.status(200).json(response);
+            console.log("Data fetched successfully");
+        }else{
+            res.status(400).json({error: "Invalid Taste Type"});
+        }
+        
+    } catch (error) {
+        console.log("Error occured while fetching data", error.message);
+        res.status(500).json({error: error.message})
+    }
+})
 
 router.post("/", async(req,res)=>{
     try {
