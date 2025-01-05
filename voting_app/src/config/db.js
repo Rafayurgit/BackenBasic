6730e668793c7c1859f85aa890e7e3e0595238cf
farruop <rafayKhanpvt@gmail.com>
2025-01-05 14:15:20 +0530

    Add: mongoDb connection

---
+++ voting_app/src/config/db.js
@@ -0,0 +1,21 @@
+import mongoose, { connect } from "mongoose";
+const mongoURL= "mongodb://localhost:27017/votingApp";
+
+mongoose.connect(mongoURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+
+db.connection= mongoose.connection;
+
+db.on("connected", ()=>{
+    console.log("connected to Mongo Server");
+})
+
+db.on("err", ()=>{
+    console.log("Error occured while connecting to server");
+})
+
+db.on("Disconnected", ()=>{
+    console.log("Mongo server disconnected");
+})
